ef1248768ecf920a6bcf6b9641f3d077f5feb3d9
Bharti kumari <bhartikumari20@navgurukul.org>
2021-10-15 14:24:01 +0530

    commit all

--- controller/getApi.controller.js
+++ controller/getApi.controller.js
@@ -3,6 +3,7 @@ const knex = require('../databaseConnection/connection')
 module.exports.showAllData = (req,res) => {
     knex.from('demo').select('*')
     .then((rows) => {
+        console.log(typeof(rows))
         res.send(rows)
         console.log(rows)
     }).catch((err) => {
